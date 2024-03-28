e868b90c32c444d1df61be55c9ae15f4b4d1e64c
Sohaib Ahsan <125086696+Sohaib-Ahsan22@users.noreply.github.com>
2024-03-28 11:13:10 -0700

    Add files via upload

---
+++ Sohaib Ahsan -InfixToPostfix.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+using namespace std;
+struct StNode
+{
+    char value;
+    StNode* next;
+};
+ 
+ StNode* top=NULL;
+
+ void push (char val)
+ {
+    StNode* temp = (StNode*) malloc (sizeof(StNode));
+        temp-> value= val;
+        temp -> next=NULL;
+        
+
+    if (top == NULL)
+    {
+        top=temp;
+    }
+    else
+    {
+        StNode* curr = top;
+        top=temp;
+        temp->next=curr;
+    }
+ }
+char head ()
+ {
+    if (top==NULL)
+    {
+        cout<< "Stack underflow"<<"\n";
+        return 0;
+    }
+    else
+    {
+    char val = top->value;
+    return val;
+    }
+ }
+ char pop ()
+ {
+    if (top==NULL)
+    {
+        cout<< "Stack underflow"<<"\n";
+        return 0;
+    }
+    else
+    {
+    StNode* curr = top;
+    char val = top->value;
+    top = top->next;
+    delete curr;
+    return val;
+    }
+ }
+int precedence(char op) 
+{
+    
+    if (op == '*' || op == '/' || op=='%')
+    {
+        return 4;
+    }
+    if (op == '+' || op == '-')
+    {
+        return 3;
+    }
+    if (op=='<' || op== '>' )
+    {
+        return 2;
+    }
+    if (op=='&' || op=='|')
+    {
+        return 1;
+    }  
+    return 0;
+}
+string PostFix(string &input)
+{
+    string postfix;
+    for (char i : input)
+    {
+        if (isalnum(i))
+        {
+            postfix += i;
+        }
+        else if (i=='(')
+        {
+           push(i); 
+        }
+        else if (i==')')
+        {
+           while (top!= NULL && head() != '(') {
+                postfix += head();
+                pop();
+            } 
+            pop();
+        }
+        else
+        {
+            while (top!= NULL && precedence(head()) >= precedence(i)) {
+                postfix += head();
+                pop();
+            }
+            push(i);
+        }
+    }
+    while (top!=NULL)
+    {
+        postfix += head();
+        pop();
+    }
+    return postfix;
+}
+int main()
+{
+    string infix;
+    cout << "Enter an infix expression: ";
+    getline(cin, infix);
+
+    string postfix = PostFix(infix);
+    cout << "Postfix expression: " << postfix << endl;
+
+    return 0;
+}
